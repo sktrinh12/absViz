bd10b98c4accada66b75b8bc78d78b1b588d637b
sktrinh12 <trinh.kcs85@gmail.com>
2019-06-29 22:03:16 -0400

    sqlite3 backend & offline-mode

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, Markup, request, jsonify, session
-from graph import corrAbs, build_graph_mongo_multiproc, build_heatmap_mongo, wellIds, dbname, lstOfwavelengths, MongoClient, chunks, lstOfPlots, os, connect_string
+# from graph import corrAbs, build_graph_mongo_multiproc, build_heatmap_mongo, wellIds, dbname, lstOfwavelengths, MongoClient, chunks, lstOfPlots, os, connect_string
+from graph import corrAbs, build_graph_multiproc, build_heatmap, wellIds, lstOfwavelengths, chunks, lstOfPlots, os, cr, tableName
 from bokeh.embed import components
 from functools import partial
 import multiprocessing
@@ -7,14 +8,18 @@ import multiprocessing
 app = Flask(__name__)
 #app.secret_key = os.urandom(10)
 #app.config['MONGODB_KEY'] = os.environ.get('MONGODB_KEY')
-client = MongoClient(connect_string)
-db = client[dbname]
+# client = MongoClient(connect_string)
+# db = client[dbname]
+# pltcodes=[fi for fi in db.list_collection_names()]
 
-pltcodes=[fi for fi in db.list_collection_names()]
-
-unqPltCodes = list(set([plt[:len(plt)-3] for plt in pltcodes]))
-unqPltCodes.sort()
+pltcodes = [plt[0] for plt in cr.execute(f"SELECT DISTINCT PLATE_CODE || SUFFIX FROM {tableName}")]
 pltcodes.sort()
+# unqPltCodes = list(set([plt[:len(plt)-3] for plt in pltcodes]))
+unqPltCodes = [plt[0] for plt in cr.execute(f"SELECT DISTINCT PLATE_CODE FROM {tableName}")]
+unqPltCodes.sort()
+
+# def replaceSVGparams(svg_string):
+#     return svg_string.replace('width="51.84pt"','width="100%"').replace('height="51.84pt"','height="100%"')
 
 @app.route('/')
 def plotgraphs():
@@ -25,7 +30,8 @@ def updateDf():
      #time2s = time.time()
      global lstOfPlots
      selected_pltcode = request.args.get('selected_pltcode').strip()
-     func_partial = partial(build_graph_mongo_multiproc,pltcodeWithSuffix=selected_pltcode)
+     # func_partial = partial(build_graph_mongo_multiproc,pltcodeWithSuffix=selected_pltcode)
+     func_partial = partial(build_graph_multiproc,pltcodeWithSuffix=selected_pltcode)
      pool = multiprocessing.Pool(processes=4)
      pool.map(func_partial,list(chunks(wellIds,100)) )
      pool.close()
@@ -36,7 +42,7 @@ def updateDf():
      #sorted_plts = extracted_vals.sort(key=lambda tup: tup[0])
      #sorted_vals = sorted(extracted_vals,key=lambda tup: tup[0])
      sorted_vals = sorted(lstOfPlots,key=lambda tup: tup[0])
-     plts = [Markup(plt[1].decode('utf-8')) for plt in sorted_vals]#.replace('width="51.84pt"','width="100%"').replace('height="51.84pt"','height="100%"')))
+     plts = [Markup(plt[1].decode('utf-8')) for plt in sorted_vals]
      #print(time.time() - time2s)
      return jsonify(htmlLinePlt=render_template('updateDF.html',lstofplots=plts),pltcode=selected_pltcode)
 
@@ -44,7 +50,7 @@ def updateDf():
 def updateHeatmap():
      selected_pltcode = request.args.get('selected_pltcode').strip()
      selected_wavelength = request.args.get('wavelength').strip()
-     p = build_heatmap_mongo(db,selected_wavelength,selected_pltcode)
+     p = build_heatmap(selected_pltcode,selected_wavelength)
      script,div = components(p)
      return jsonify(htmlHeatmap=render_template('updateHeatmap.html',script=script,div=div),pltcode=selected_pltcode,selected_wavelength=selected_wavelength)
 
@@ -54,9 +60,9 @@ def updateCorrel():
     selected_wavelength = int(request.args.get('wavelength2').strip())
     pltcode100 = selected_pltcode + '100'
     pltcode200 = selected_pltcode + '200'
-    corrcoeff = corrAbs(db,pltcode100,pltcode200,selected_wavelength)
+    corrcoeff = corrAbs(cr,pltcode100,pltcode200,selected_wavelength) #originally had db for mongodb
     return jsonify(htmlCorr=render_template('updateCorrl.html', corrcoeff=corrcoeff))
 
 if __name__ == '__main__':
     app.debug = True
-    app.run()
+    app.run(host="0.0.0.0", port=8000)
--- graph.py
+++ graph.py
@@ -5,13 +5,16 @@ from bokeh.plotting import figure
 from bokeh.models import HoverTool
 import io
 import numpy as np
-from pymongo import MongoClient
+# from pymongo import MongoClient
 import multiprocessing
 import os
+import sqlite3
 
-dbname = 'ncifred'
-#connect_string = os.environ.get('MONGODB_KEY') for local
-connect_string = f"mongodb+srv://{os.environ['MONGODB_KEY']}:{os.environ['MONGODB_SECRET']}@ncifrederick-l7ves.mongodb.net/ncifred?retryWrites=true" #for heroku
+conn = sqlite3.connect('/Users/spencertrinh/GitRepos/absViz/NCIFrederick/static/abs384QCdb.db')
+tableName = "QCabsVals"
+cr = conn.cursor()
+# dbname = 'ncifred'
+# connect_string = f"mongodb+srv://{os.environ['MONGODB_KEY']}:{os.environ['MONGODB_SECRET']}@ncifrederick-l7ves.mongodb.net/ncifred?retryWrites=true" 
 lstOfwavelengths = list(range(220,810,10))
 manager = multiprocessing.Manager()
 lstOfPlots = manager.list()
@@ -29,91 +32,115 @@ def chunks(l, n):
     for i in range(0, len(l), n):
         yield l[i:i + n]
 
-def getWavelengthData(db,pltcodeWithSuffix,wavelength):
-    ''' Return dicitonary of wellids and their absorbance values '''
-    res=db[pltcodeWithSuffix].find({"Wavelength":wavelength})
-    return {k:v for k,v in res[0].items() if k not in ['_id','Wavelength','Temperature(¡C)']}
+# def getWavelengthData_mg(db,pltcodeWithSuffix,wavelength):
+#     ''' Return dicitonary of wellids and their absorbance values '''
+#     res=db[pltcodeWithSuffix].find({"Wavelength":wavelength})
+#     return {k:v for k,v in res[0].items() if k not in ['_id','Wavelength','Temperature(¡C)']}
 
-def getAllWellVals(db,pltcodeWithSuffix,wellID):
-    lstOfVals = []
-    for absnum in db[pltcodeWithSuffix].find({}, {wellID: 1 , '_id': 0}):
-        lstOfVals.append(absnum[wellID ])
-    return lstOfVals
+def getWavelengthData(cr,tableName,pltcodeWithSuffix,wv):
+    pltcode = pltcodeWithSuffix[:8]
+    suffix = pltcodeWithSuffix[8:11]
+    datadict = {}
+    for k,v in cr.execute(f'''SELECT WELLID,ABS FROM {tableName} WHERE PLATE_CODE = '{pltcode}' and WAVELENGTH = {wv} AND SUFFIX = '{suffix}' '''):
+        datadict[k] = v
+    return datadict
 
-def build_graph_mongo_multiproc(chunk,pltcodeWithSuffix):
+def getAllWellVals(cr,tableName,pltcodeWithSuffix,wellID):
+    pltcode = pltcodeWithSuffix[:8]
+    suffix = pltcodeWithSuffix[8:11]
+    return [r for r in cr.execute(f'''SELECT ABS FROM '{tableName}' WHERE WELLID =
+            '{wellID}' and PLATE_CODE = '{pltcode}' AND SUFFIX = '{suffix}' ''')]
+
+# def getAllWellVals_mg(db,pltcodeWithSuffix,wellID):
+#     lstOfVals = []
+#     for absnum in db[pltcodeWithSuffix].find({}, {wellID: 1 , '_id': 0}):
+#         lstOfVals.append(absnum[wellID ])
+#     return lstOfVals
+
+# def build_graph_mongo_multiproc(chunk,pltcode): 
+def build_graph_multiproc(chunk,pltcodeWithSuffix):
     global lstOfPlots
-    client=MongoClient(connect_string,maxPoolSize=10000)
-    db = client[dbname]
-    #loop over the id's in the chunk and do the plotting with each
-    for i, wid in chunk:
-        #do the plotting with document collection.find_one(id)
-        img = io.BytesIO()
-        fig = Figure(figsize=(0.6,0.6))
-        axis = fig.add_subplot(1,1,1)
-        absVals = getAllWellVals(db,pltcodeWithSuffix,wid)
-        axis.plot(lstOfwavelengths,absVals)
-        axis.set_title(f'{wid}',fontsize=9)
-        axis.title.set_position([.5, .6])
-        axis.tick_params(
-                which='both',
-                bottom=False,
-                left=False,
-                labelbottom=False,
-                labelleft=False)
-        FigureCanvasSVG(fig).print_svg(img)
-        result = img.getvalue()
-        try:
-            lstOfPlots[i] = (i,result)
-        except IndexError:
-            lstOfPlots.append((i,result))
+    #client=MongoClient(connect_string,maxPoolSize=10000)
+    #db = client[dbname]
+    with sqlite3.connect('/Users/spencertrinh/GitRepos/absViz/NCIFrederick/static/abs384QCdb.db') as conn_:
+        cr_ = conn_.cursor()
+        #loop over the id's in the chunk and do the plotting with each
+        for i, wid in chunk:
+            #do the plotting with document collection.find_one(id) MongoDB
+            img = io.BytesIO()
+            fig = Figure(figsize=(0.6,0.6))
+            axis = fig.add_subplot(1,1,1)
+            absVals = getAllWellVals(cr_,tableName,pltcodeWithSuffix,wid)
+            axis.plot(lstOfwavelengths,absVals)
+            axis.set_title(f'{wid}',fontsize=9)
+            axis.title.set_position([.5, .6])
+            axis.tick_params(
+                    which='both',
+                    bottom=False,
+                    left=False,
+                    labelbottom=False,
+                    labelleft=False)
+            FigureCanvasSVG(fig).print_svg(img)
+            result = img.getvalue()
+            try:
+                lstOfPlots[i] = (i,result)
+            except IndexError:
+                lstOfPlots.append((i,result))
 
-def build_heatmap_mongo(db,wavelength,pltcodeWithSuffix):
-    datadict = getWavelengthData(db,pltcodeWithSuffix,int(wavelength))
-    vals=[]
-    for i in db[pltcodeWithSuffix].find({}):
-        for k,v in i.items():
-            if k not in ['_id','Wavelength','Temperature(¡C)']:
-                vals.append(v) 
-    max_val = np.array(vals).max()
-    min_val = abs(np.array(vals)).min()
-    data_array = np.array(list(datadict.values())).reshape(16,24).T
-    colourBlue = [ '#E3E6E8', '#E0E7EB', '#DEE8ED', '#DBE9F0', '#D9EAF2', '#D6EBF5', '#D4EBF7', '#D1ECFA', '#CFEDFC', '#CCEEFF', '#A8D8F0', '#A3DAF5', '#9EDBFA', '#99DDFF', '#7DC4E8', '#75C7F0', '#6EC9F7', '#66CCFF' ]
-    nphist = np.linspace(min_val,max_val,num=len(colourBlue))
-    lamDiff = lambda x: [abs(x-i) for i in nphist].index(min([abs(x-i) for i in nphist]))
-    colours=[]
-    for i in range(data_array.shape[0]):
-        for j in range(data_array.shape[1]):
-            if data_array[i,j] < 0:
-                colours.append('#5C6970')
-            else:
-                colours.append(colourBlue[lamDiff(data_array[i,j])])
-    xs = list(map(lambda x: [x]*16,list(range(24))))
-    xs = [str(item+1) for sublist in xs for item in sublist] #flatten list
-    strings = [i for i in string.ascii_uppercase[0:16]]*24
-    df = {'xs':xs,'ys':strings,'value':data_array.flatten(),'colour':colours}
-    p = figure(plot_width=1600,plot_height=1200,x_axis_location="above", tools="hover",
-               sizing_mode='scale_width',
-               x_range=[str(i) for i in range(1,25)],
-               y_range=list(reversed([i for i in string.ascii_uppercase[:16]])),
-               tooltips = [('wellID', '@ys,@xs'), ('abs', '@value')])
-    p.rect('xs', 'ys', .64,.64 ,source=df, fill_color='colour',line_color='black')
-    p.toolbar.logo = None
-    p.toolbar_location = None
-    p.xgrid.grid_line_color = None
-    p.ygrid.grid_line_color = None
-    return p
+# def build_heatmap_mongo(db,pltcode,wavelength):
+def build_heatmap(pltcodeWithSuffix,wavelength):
+    with sqlite3.connect('/Users/spencertrinh/GitRepos/absViz/NCIFrederick/static/abs384QCdb.db') as conn_:
+        cr_ = conn_.cursor()
+        datadict = getWavelengthData(cr_,tableName, pltcodeWithSuffix,int(wavelength))
+        # vals=[]
+        # for i in db[pltcodeWithSuffix].find({}):
+        #     for k,v in i.items():
+        #         if k not in ['_id','Wavelength','Temperature(¡C)']:
+        #             vals.append(v) 
+        # max_val = np.array(vals).max()
+        # min_val = abs(np.array(vals)).min()
+        max_val = np.array([v for v in datadict.values()]).max()
+        min_val = abs(np.array([v for v in datadict.values()])).min()
+        data_array = np.array(list(datadict.values())).reshape(16,24).T
+        colourBlue = [ '#E3E6E8', '#E0E7EB', '#DEE8ED', '#DBE9F0', '#D9EAF2', '#D6EBF5', '#D4EBF7', '#D1ECFA', '#CFEDFC', '#CCEEFF', '#A8D8F0', '#A3DAF5', '#9EDBFA', '#99DDFF', '#7DC4E8', '#75C7F0', '#6EC9F7', '#66CCFF' ]
+        nphist = np.linspace(min_val,max_val,num=len(colourBlue))
+        lamDiff = lambda x: [abs(x-i) for i in nphist].index(min([abs(x-i) for i in nphist]))
+        colours=[]
+        for i in range(data_array.shape[0]):
+            for j in range(data_array.shape[1]):
+                if data_array[i,j] < 0:
+                    colours.append('#5C6970')
+                else:
+                    colours.append(colourBlue[lamDiff(data_array[i,j])])
+        xs = list(map(lambda x: [x]*16,list(range(24))))
+        xs = [str(item+1) for sublist in xs for item in sublist] #flatten list
+        strings = [i for i in string.ascii_uppercase[0:16]]*24
+        df = {'xs':xs,'ys':strings,'value':data_array.flatten(),'colour':colours}
+        p = figure(plot_width=1600,plot_height=1200,x_axis_location="above", tools="hover",
+                   sizing_mode='scale_width',
+                   x_range=[str(i) for i in range(1,25)],
+                   y_range=list(reversed([i for i in string.ascii_uppercase[:16]])),
+                   tooltips = [('wellID', '@ys,@xs'), ('abs', '@value')])
+        p.rect('xs', 'ys', .64,.64 ,source=df, fill_color='colour',line_color='black')
+        p.toolbar.logo = None
+        p.toolbar_location = None
+        p.xgrid.grid_line_color = None
+        p.ygrid.grid_line_color = None
+        return p
 
 
 def corrAbs(db,pltcodeWithSuffix1,pltcodeWithSuffix2,wavelength):
-    crude = list(getWavelengthData(db,pltcodeWithSuffix1,wavelength).values())
-    fx = list(getWavelengthData(db,pltcodeWithSuffix2,wavelength).values())
-    darray_crude = np.array(crude).reshape(16,24)
-    darray_fx = np.array(fx).reshape(16,24)
-    darray_t = np.flip(np.flip(darray_crude.T,axis=1),axis=0).reshape(16,24) #flip the 100 plate
-    ar_crude = np.delete(np.delete(np.delete(darray_crude, np.s_[1::2], 1),[0],1),np.s_[1::2],0) #filter elements so that only obtain the first value of each quadrant
-    ar_crude_t = np.delete(np.delete(np.delete(darray_t, np.s_[1::2], 1),[0],1),np.s_[1::2],0) #filter elements as above but for tranposed plate
-    ar_fx7 = np.delete(np.delete(np.delete(darray_fx, np.s_[::2], 1),[0],1),np.s_[0::2],0) #filter elemnts for 7th fx
-    ar_fx6 = np.delete(np.delete(np.delete(darray_fx, np.s_[1::2], 1),[0],1),np.s_[0::2],0)
-    corr6 = (np.corrcoef(ar_crude.flatten(),ar_fx6.flatten())[0][1],np.corrcoef(ar_crude_t.flatten(),ar_fx6.flatten())[0][1])
-    corr7 = (np.corrcoef(ar_crude.flatten(),ar_fx7.flatten())[0][1],np.corrcoef(ar_crude_t.flatten(),ar_fx7.flatten())[0][1])
-    return (corr6,corr7)
+    with sqlite3.connect('/Users/spencertrinh/GitRepos/absViz/NCIFrederick/static/abs384QCdb.db') as conn_:
+        cr_ = conn_.cursor()
+        crude = list(getWavelengthData(cr_,tableName,pltcodeWithSuffix1,wavelength).values())
+        fx = list(getWavelengthData(cr_,tableName,pltcodeWithSuffix2,wavelength).values())
+        darray_crude = np.array(crude).reshape(16,24)
+        darray_fx = np.array(fx).reshape(16,24)
+        darray_t = np.flip(np.flip(darray_crude.T,axis=1),axis=0).reshape(16,24) #flip the 100 plate
+        ar_crude = np.delete(np.delete(np.delete(darray_crude, np.s_[1::2], 1),[0],1),np.s_[1::2],0) #filter elements so that only obtain the first value of each quadrant
+        ar_crude_t = np.delete(np.delete(np.delete(darray_t, np.s_[1::2], 1),[0],1),np.s_[1::2],0) #filter elements as above but for tranposed plate
+        ar_fx7 = np.delete(np.delete(np.delete(darray_fx, np.s_[::2], 1),[0],1),np.s_[0::2],0) #filter elemnts for 7th fx, the 4th value of each quadrant (backwards 'C')
+        ar_fx6 = np.delete(np.delete(np.delete(darray_fx, np.s_[1::2], 1),[0],1),np.s_[0::2],0)
+        corr6 = (np.corrcoef(ar_crude.flatten(),ar_fx6.flatten())[0][1],np.corrcoef(ar_crude_t.flatten(),ar_fx6.flatten())[0][1])
+        corr7 = (np.corrcoef(ar_crude.flatten(),ar_fx7.flatten())[0][1],np.corrcoef(ar_crude_t.flatten(),ar_fx7.flatten())[0][1])
+        return (corr6,corr7)
